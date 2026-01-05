4e6811dd1f73df4e694882f366fd981007d9a898
samsincee04 <osiakpe1@charlotte.edu>
2026-01-05 14:22:25 -0500

    Update STM_interrupt_ctrl.c

--- STM_interrupt_ctrl.c
+++ STM_interrupt_ctrl.c
@@ -1,12 +1,52 @@
 // STM32F091RC - GPIOC buttons on PC6 & PC8; LEDs on PC4 & PC5
 // Build with stm32f0xx.h available (STM32CubeIDE or equivalent toolchain)
 /**
- * Youtube link: https://youtube.com/shorts/VzZSHi-zsfE?si=_T_5k3jEHORVmxJb
+ * Youtube link: https://youtu.be/Lcb9Z_0MaTg?si=sfyK244dzoF2S-nW
  */
 
 #include "stm32f0xx.h"
 #include <stdint.h>
 #include <stdbool.h>
+
+//          GPIO CONFIG
+
+// LED pin (PA5)
+#define LED_GPIO_PORT          GPIOA
+#define LED_PIN_NUM            5u
+#define LED_PIN_MASK           (1u << LED_PIN_NUM)
+
+// Button pin (PC13)
+#define BTN_GPIO_PORT          GPIOC
+#define BTN_PIN_NUM            13u
+#define BTN_PIN_MASK           (1u << BTN_PIN_NUM)
+
+// MODER/AFR bit shifts
+#define GPIO_MODER_BITS_PER_PIN   2u
+#define GPIO_AFR_BITS_PER_PIN     4u
+
+// MODER mode values
+#define GPIO_MODE_INPUT           0x0u
+#define GPIO_MODE_OUTPUT          0x1u
+#define GPIO_MODE_AF              0x2u
+#define GPIO_MODE_ANALOG          0x3u
+
+// PUPDR values
+#define GPIO_PULL_NONE            0x0u
+#define GPIO_PULL_UP              0x1u
+#define GPIO_PULL_DOWN            0x2u
+
+//               EXTI CONFIG
+
+#define BTN_EXTI_LINE             BTN_PIN_NUM
+#define BTN_EXTI_MASK             (1u << BTN_EXTI_LINE)
+
+#define EXTI_PORTC_VALUE          0x2u     // Port C value for EXTICR
+#define EXTICR_INDEX              (BTN_PIN_NUM / 4u)
+#define EXTICR_SHIFT              ((BTN_PIN_NUM % 4u) * 4u)
+
+//       APPLICATION CONSTANTS
+
+#define NUM_STATES                3u
 
 #define SYSTICK_CLK_FREQ_HZ     48000000ul // 48MHz for STM32F091
 #define SYSTICK_TICK_MS         1ul        // 1ms tick rate
@@ -14,14 +54,18 @@
 
 #define BLINK_INTERVAL_MS       500ul      // Toggle every 500ms (1Hz blink rate)
 #define DEBOUNCE_DELAY_MS       30ul       // 30ms minimum time between button presses
+#define INACTIVITY_TIME_OUT_MS  5000ul
 
 
 typedef enum { STATE_0, STATE_1, STATE_2} state_t;
 
 static void gpio_init(void);
 static void systick_init(void);
+static void enter_stop_mode(void);
+
 void EXTI4_15_IRQHandler(void);
 void SysTick_Handler(void);
+
 static inline void setLed(bool enable);
 static inline void toggleLed(void);
 
@@ -32,13 +76,20 @@ volatile uint32_t g_last_press_time = 0;
 
 int main(void){
 
-
 uint32_t last_toggle_time = 0;
+uint32_t last_activity_time = 0;
 
 gpio_init();
 systick_init();
 
 while(1) {
+	//Enter STOP mode after inactivity
+	if((g_tick_count - last_activity_time) > INACTIVITY_TIME_OUT_MS){
+
+		enter_stop_mode();
+		systick_init();
+		last_activity_time = g_tick_count;
+	}
 switch(mode){
 
 case STATE_0:
@@ -71,43 +122,55 @@ static void gpio_init(void){
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	RCC -> AHBENR |= RCC_AHBENR_GPIOCEN;
 
-	GPIOA->MODER &= ~(0x3u << (5 * 2));
-	GPIOA->MODER |= (0x1u << (5 * 2));
+	// LED: PA5 Output
+	LED_GPIO_PORT->MODER &= ~(0x3u << (LED_PIN_NUM * GPIO_MODER_BITS_PER_PIN));
+	LED_GPIO_PORT->MODER |= (GPIO_MODE_OUTPUT << (LED_PIN_NUM * GPIO_MODER_BITS_PER_PIN));
 
 	    // Configure PC13 as input and enable pull-up
-	GPIOC -> MODER &= ~(0x3u << (13 * 2));// PC13 as input
-    GPIOC -> PUPDR &= ~(0x3u << (13 * 2));
-    GPIOC -> PUPDR |= (0x1u << (13 * 2));
+	BTN_GPIO_PORT -> MODER &= ~(0x3u << (BTN_PIN_NUM *  GPIO_MODER_BITS_PER_PIN));// PC13 as input
+	BTN_GPIO_PORT -> PUPDR &= ~(0x3u << (BTN_PIN_NUM *  GPIO_MODER_BITS_PER_PIN));
+	BTN_GPIO_PORT -> PUPDR |= (GPIO_PULL_UP << (BTN_PIN_NUM *  GPIO_MODER_BITS_PER_PIN));
 
-    RCC -> APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;// enable SYSCFG clk
+    //    EXTI Configuration
+	RCC -> APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;// enable SYSCFG clk
 
-    SYSCFG -> EXTICR[3] &= ~(0xFu << (1 * 4));
-    SYSCFG -> EXTICR[3] |= (0x2u << (1 * 4));
+    SYSCFG -> EXTICR[EXTICR_INDEX] &= ~(0xFu << EXTICR_SHIFT);
+    SYSCFG -> EXTICR[EXTICR_INDEX] |= (EXTI_PORTC_VALUE << EXTICR_SHIFT);
 
 
-    EXTI -> FTSR |= (0x1u << (1 * 13));// falling edge is active
-    EXTI -> RTSR &= ~(0x1u << (1 * 13));// ensure only falling edge is active
-    EXTI -> IMR |= (0x1u << (1 * 13));
-    EXTI -> PR |= (0x1u << (1 * 13));
+    EXTI -> FTSR |= BTN_EXTI_MASK;// falling edge is active
+    EXTI -> RTSR &= ~BTN_EXTI_MASK;// ensure only falling edge is active
+    EXTI -> IMR |= BTN_EXTI_MASK;
+    EXTI -> PR |= BTN_EXTI_MASK;
 
     NVIC_EnableIRQ(EXTI4_15_IRQn);
+}
+
+//STOP MODE Entry (Deep Sleep)
+static void enter_stop_mode (void){
+	SysTick -> CTRL &= ~SysTick_CTRL_ENABLE_Msk; // disable SysTick
+
+	SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;  // select STOP mode
+
+	__WFI(); // Enter STOP mode
+
 }
 
 
 void EXTI4_15_IRQHandler(void) {
         // Check if EXTI line 13 triggered the interrupt
-        if (EXTI->PR & (0x1ul << 13)) {
+        if (EXTI->PR & BTN_EXTI_MASK) {
 
         	if ((g_tick_count - g_last_press_time) > DEBOUNCE_DELAY_MS) {
         	            // State Transition: 0 -> 1 -> 2 -> 0
-        	            mode = (mode + 1) % 3;
+        	            mode = (mode + 1) % NUM_STATES;
 
         	            // Update the last press time
         	            g_last_press_time = g_tick_count;
         	        }
 
 
-        	EXTI->PR |= (0x1ul << 13);
+        	EXTI->PR |= BTN_EXTI_MASK; // Clear interrupt flag
 
         }
 
@@ -117,15 +180,15 @@ void EXTI4_15_IRQHandler(void) {
 
 static inline void setLed(bool enable){
 	if(enable){
-		GPIOA->BSRR = (1u << 5); //set PA5(turn LED on)
+	   LED_GPIO_PORT->BSRR = LED_PIN_MASK; //set PA5(turn LED on)
 	}else{
-			GPIOA->BSRR = (1u << (5 + 16)); // turn LED off
+	   LED_GPIO_PORT->BSRR = (LED_PIN_MASK << 16u); // turn LED off
 		}
 	}
 
 static inline void toggleLed(void){
 
-	    GPIOA->ODR ^= (1u << 5);
+	    GPIOA->ODR ^= LED_PIN_MASK;
 }
 
 
@@ -142,5 +205,4 @@ static void systick_init(void){
 	    // Enable SysTick
 	    SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
 }
-
 
